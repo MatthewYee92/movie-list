e470feaab16b37341a6d24faa5709bdbf85bb597
Matthew Yee <matthewyee@Matthews-MacBook-Pro.local>
2019-11-11 21:15:52 -0800

    added express server example

---
+++ server/server.js
@@ -0,0 +1,8 @@
+// example express server
+const express = require('express')
+const app = express
+const PORT = 3000
+
+app.get('/', (req, res) => res.send(`Hello World`))
+
+app.listen(PORT, `listening on port ${PORT}`)
